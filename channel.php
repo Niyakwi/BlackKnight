b567b77cf40bc0528b9477863976460b3ae6154d
Niyakwi <nikitatopor93@gmail.com>
2026-03-05 00:24:36 +0300

    Update channel.php

--- channel.php
+++ channel.php
@@ -1,88 +1,23 @@
 <?php
-$telegramChannelURLs = [
-    "https://t.me/s/networknim",
-    "https://t.me/s/beiten",
-    "https://t.me/s/MsV2ray",
-    "https://t.me/s/foxrayiran",
-    "https://t.me/s/DailyV2RY",
-    "https://t.me/s/yaney_01",
-    "https://t.me/s/FreakConfig",
-    "https://t.me/s/EliV2ray",
-    "https://t.me/s/ServerNett",
-    "https://t.me/s/proxystore11",
-    "https://t.me/s/v2rayng_fa2",
-    "https://t.me/s/v2rayng_org",
-    "https://t.me/s/V2rayNGvpni",
-    "https://t.me/s/custom_14",
-    "https://t.me/s/v2rayNG_VPNN",
-    "https://t.me/s/v2ray_outlineir",
-    "https://t.me/s/v2_vmess",
-	"https://t.me/s/FreeVlessVpn",
-	"https://t.me/s/vmess_vless_v2rayng",
-	"https://t.me/s/PrivateVPNs",
-	"https://t.me/s/freeland8",
-	"https://t.me/s/vmessiran",
-	"https://t.me/s/Outline_Vpn",
-	"https://t.me/s/vmessq",
-	"https://t.me/s/WeePeeN",
-	"https://t.me/s/V2rayNG3",
-	"https://t.me/s/ShadowsocksM",
-	"https://t.me/s/shadowsocksshop",
-	"https://t.me/s/v2rayan",
-	"https://t.me/s/ShadowSocks_s",
-	"https://t.me/s/VmessProtocol",
-	"https://t.me/s/napsternetv_config",
-	"https://t.me/s/Easy_Free_VPN",
-	"https://t.me/s/V2Ray_FreedomIran",
-	"https://t.me/s/V2RAY_VMESS_free",
-	"https://t.me/s/v2ray_for_free",
-	"https://t.me/s/V2rayN_Free",
-	"https://t.me/s/free4allVPN",
-	"https://t.me/s/vpn_ocean",
-	"https://t.me/s/configV2rayForFree",
-	"https://t.me/s/FreeV2rays",
-	"https://t.me/s/DigiV2ray",
-	"https://t.me/s/v2rayNG_VPN",
-	"https://t.me/s/freev2rayssr",
-	"https://t.me/s/v2rayn_server",
-	"https://t.me/s/Shadowlinkserverr",
-	"https://t.me/s/iranvpnet",
-	"https://t.me/s/vmess_iran",
-	"https://t.me/s/mahsaamoon1",
-	"https://t.me/s/V2RAY_NEW",
-	"https://t.me/s/v2RayChannel",
-	"https://t.me/s/configV2rayNG",
-	"https://t.me/s/config_v2ray",
-	"https://t.me/s/vpn_proxy_custom",
-	"https://t.me/s/vpnmasi",
-	"https://t.me/s/v2ray_custom",
-	"https://t.me/s/VPNCUSTOMIZE",
-	"https://t.me/s/HTTPCustomLand",
-	"https://t.me/s/vpn_proxy_custom",
-	"https://t.me/s/ViPVpn_v2ray",
-	"https://t.me/s/FreeNet1500",
-	"https://t.me/s/v2ray_ar",
-	"https://t.me/s/beta_v2ray",
-	"https://t.me/s/vip_vpn_2022",
-	"https://t.me/s/FOX_VPN66",
-	"https://t.me/s/VorTexIRN",
-	"https://t.me/s/YtTe3la",
-	"https://t.me/s/V2RayOxygen",
-	"https://t.me/s/Network_442",
-	"https://t.me/s/VPN_443",
-	"https://t.me/s/v2rayng_v",
-	"https://t.me/s/ultrasurf_12",
-	"https://t.me/s/iSeqaro",
-	"https://t.me/s/frev2rayng",
-	"https://t.me/s/frev2ray",
-	"https://t.me/s/FreakConfig",
-	"https://t.me/s/Awlix_ir",
-	"https://t.me/s/v2rayngvpn",
-	"https://t.me/s/God_CONFIG",
-	"https://t.me/s/Configforvpn01",
-	"https://t.me/s/inikotesla",
-	"https://t.me/s/forwardv2ray",
-    "https://t.me/s/TUICity",
-    "https://t.me/s/ParsRoute",
-];
+// Сначала пытаемся получить список каналов из переменной окружения MY_CHANNELS
+$envChannels = getenv('MY_CHANNELS');
+
+if ($envChannels) {
+    // Разбиваем строку из переменной окружения по переводу строки
+    $channels = explode("\n", trim($envChannels));
+    // Удаляем пустые строки и пробелы по краям
+    $channels = array_map('trim', $channels);
+    $channels = array_filter($channels);
+} else {
+    // Если переменной нет, используем стандартный список каналов (можно оставить пустым или задать свои)
+    $channels = [
+        // Примеры каналов, можно оставить как есть или удалить
+        'https://t.me/some_public_channel_1',
+        'https://t.me/some_public_channel_2',
+    ];
+}
+
+// Преобразуем в формат, который ожидает основной скрипт (например, URL каналов)
+// Предполагается, что основной скрипт использует этот массив для парсинга
+return $channels;
 ?>
