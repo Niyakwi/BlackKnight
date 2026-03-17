b1287349d9b68910ff7ae7647ca71c009e3326cf
Niyakwi <nikitatopor93@gmail.com>
2026-03-17 08:32:22 +0300

    Update channel.php

--- channel.php
+++ channel.php
@@ -1,18 +1,5 @@
 <?php
-// Получаем список каналов из секрета MY_CHANNELS (переменная окружения)
-$envChannels = getenv('MY_CHANNELS');
-
-if ($envChannels) {
-    // Разбиваем по строкам, убираем пустые и лишние пробелы
-    $telegramChannelURLs = explode("\n", trim($envChannels));
-    $telegramChannelURLs = array_map('trim', $telegramChannelURLs);
-    $telegramChannelURLs = array_filter($telegramChannelURLs);
-} else {
-    // Если секрет не задан – используем запасной список (можно оставить пустым)
-    $telegramChannelURLs = [
-        'https://t.me/some_public_channel_1',
-        'https://t.me/some_public_channel_2',
-    ];
-}
-// Никакого return! Просто оставляем переменную определённой
-?>
+$telegramChannelURLs = [
+    "https://t.me/akhbare2025",
+    " https://t.me/duckvp_n",
+    
